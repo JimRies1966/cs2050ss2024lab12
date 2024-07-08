0906380ec672f356fb743a6a605af18d3e3155e8
Jim Ries <RiesJ@missouri.edu>
2024-07-08 15:31:16 -0500

    Initial roll-in

---
+++ lab12.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include "lab12.h"
+
+PHeap HeapConstruct()
+{
+	return NULL;
+}
+
+int HeapInsert(PHeap heap, int key, void * inputData)
+{
+	return -1;
+}
+
+
+int HeapDelete(PHeap heap, void ** outputData)
+{
+	return -1;
+}
+
+
+void HeapDestruct(PHeap heap)
+{
+}
